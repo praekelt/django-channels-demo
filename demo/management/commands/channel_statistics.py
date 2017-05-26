cfae022ccaa30b99bfb321bdd11ab39549613c25
cobusc <cobus.carstens@gmail.com>
2017-05-26 13:55:42 +0200

    Use the 'right' way to access the info

--- demo/management/commands/channel_statistics.py
+++ demo/management/commands/channel_statistics.py
@@ -3,19 +3,16 @@ from pprint import pprint
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
-
+from channels.asgi import get_channel_layer
 
 class Command(BaseCommand):
     help = "Display Django channels global statistics"
 
     def handle(self, *args, **kwargs):
-        backend_class_path = settings.CHANNEL_LAYERS["default"]["BACKEND"]
-        module_name, class_name = backend_class_path.rsplit(".", 1)
-        module = importlib.import_module(module_name)
-        klass = getattr(module, class_name)
-        instance = klass()
-        pprint(instance.global_statistics())
-        print("Global statistics expiry: {}".format(
-            instance.global_statistics_expiry)
-        )
+        layer = get_channel_layer()
+        if "statistics" in layer.extensions:
+            pprint(layer.global_statistics())
+        else:
+            print("The statistics extension is not supported")
 
+        pprint(dir(layer))
